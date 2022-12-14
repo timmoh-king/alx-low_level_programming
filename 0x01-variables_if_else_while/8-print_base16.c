d577aab87f088072eab674cc71d1a8d6dc49d376
timmoh-king <timothykariuki43@gmail.com>
2022-12-14 10:52:06 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints all the numbers of
+ * base 16 in lowercase followed by a new line
+ *
+ * Return: Success 0 (Always)
+ */
+int main(void)
+{
+	int base_16;
+	char letter;
+
+	for (base_16 = 0; base_16 < 16; base_16++)
+		putchar((num % 10) + '0');
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
