2f911b691af747433a4e8301291838762720e82c
timmoh-king <timothykariuki43@gmail.com>
2022-12-19 01:20:01 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: The character to be checked
+ * @b: The character to be checked
+ *
+ * Return: Integer
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
