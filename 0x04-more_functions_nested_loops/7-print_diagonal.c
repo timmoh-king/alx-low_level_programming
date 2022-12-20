2c1d5716335b8639b8ffe600dcadb947f3fe9adc
timmoh-king <timothykariuki43@gmail.com>
2022-12-20 02:42:06 -0800

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,14 +8,23 @@
  */
 void print_diagonal(int n)
 {
-	int len;
+	int len, space;
 
 	if (n > 0)
 	{
 		for (len = 0; len < n; len++)
-			_putchar('\');
-	}
+		{
+			for (space = 0; space < len; space++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\')
 
+			if (len == n - 1)
+				continue;
+
+			_putchar('\n');
+		}
+	}
 	_putchar('\n');
 }
-
