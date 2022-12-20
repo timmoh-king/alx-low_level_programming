279431a7474e38f2f1cf0743c94015b4f3a0fae4
timmoh-king <timothykariuki43@gmail.com>
2022-12-20 08:37:03 -0800

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - a function that prints a triangle
+ * @size: The character to be checked
+ *
+ * Return: return newline
+ */
+void print_triangle(int size)
+{
+	int len, space;
+
+	if (size > 0)
+	{
+		for (len = 0; len < size; len++)
+		{
+			for (space = size - len; space > 0; space--)
+			{
+				_putchar(' ');
+			}
+			for (space = 0; space < len; space++)
+			{
+				_putchar('#');
+			}
+
+			if (len == size)
+				continue;
+
+			_putchar('\n');
+	}
+
+	_putchar('\n');
+}
