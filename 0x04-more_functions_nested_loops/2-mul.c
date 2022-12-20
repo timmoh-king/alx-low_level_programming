52d954bd3846693e5a60a99ef16e914a75e294c5
timmoh-king <timothykariuki43@gmail.com>
2022-12-20 01:08:45 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - a function that multiplies two integers
+ * @a: Character to be checked
+ * @b: Character to be checked
+ *
+ * Return: integer value
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
