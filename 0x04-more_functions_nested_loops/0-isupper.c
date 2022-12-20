e036a85b04910d5e61638e6dd5c285077e7de44c
timmoh-king <timothykariuki43@gmail.com>
2022-12-20 00:27:00 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to be checked
+ *
+ * Return: 1 if uppercase and 0 otherwise
+ */
+int _isupper(int c)
+{
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			putchar("%d", c);
+			putchar(':');
+			putchar('\n');
+
+			return (0);
+		}
+
+		putchar("%d", c);
+		putchar(':');
+		putchar('\n');
+
+		return (1);
+	}
+}
