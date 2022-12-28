bfe8ed01c1cce9d144a3e1d3041b813304dd799c
timmoh-king <timothykariuki43@gmail.com>
2022-12-27 23:29:25 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen -  returns the length of a string
+ * @s: The character to be checked
+ *
+ * Return: The integer value
+ */
+int _strlen(char *s)
+{
+	char b[];
+
+	s = sizeof(b);
+
+	return (s);
+}
