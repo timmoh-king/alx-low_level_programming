7b50e6dfca0b25d0b077a0b2878b4ef4894c47a6
timmoh-king <timothykariuki43@gmail.com>
2023-01-15 04:09:20 -0800

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,10 +19,16 @@ int **alloc_grid(int width, int height)
 	ptr = malloc(height * sizeof(int *));
 
 	if (ptr == NULL)
+		free(ptr);
 		return (NULL);
 
 	for (i = 0; i < height; i++)
+	{
 		ptr[i] = malloc(width * sizeof(int));
+
+		if (ptr[i] == NULL)
+			free(ptr[i]);
+	}
 
 	return (ptr);
 }
