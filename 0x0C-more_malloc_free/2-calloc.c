db8630a19ff34120122000f437340ac5625e4b55
timmoh-king <timothykariuki43@gmail.com>
2023-01-17 09:08:23 -0800

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,15 +10,21 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int **ptr;
+	char *ptr;
+	unsigned int i;
 
-	if (nmemb == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ptr = malloc(sizeof(size) * nmemb);
+	ptr = malloc(size * nmemb);
 
 	if (ptr == NULL)
 		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		ptr[i] = 0;
+	}
 
 	return (ptr);
 }
