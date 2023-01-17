6d225d21a59c9c1056ee145b7fcad8a41f700e66
timmoh-king <timothykariuki43@gmail.com>
2023-01-17 08:57:02 -0800

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,10 +24,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	len1 = strlen(s1);
 	len2 = strlen(s2);
 
-	if (n >= len2)
+	if (n > len2)
 		n = len2;
 
-	len3 = len1 + len2 + 1;
+	len3 = len1 + n + 1;
 
 	ptr = malloc(len3 * sizeof(char));
 
