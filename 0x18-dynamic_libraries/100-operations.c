80a2155fd401e8d4978dccad3d5e354cb68d7c3e
timmoh-king <timothykariuki43@gmail.com>
2023-03-21 03:26:43 -0700

    files

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+/**
+ * sum - add numbers
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - add numbers
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: sum
+ */
+int sub(int a, int b)
+{
+        return (a - b);
+}
+/**
+ * div - add numbers
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: sum
+ */
+int div(int a, int b)
+{
+        return (a / b);
+}
+/**
+ * mul - add numbers
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: sum
+ */
+int mul(int a, int b)
+{
+        return (a * b);
+}
+/**
+ * mod - add numbers
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: sum
+ */
+int mod(int a, int b)
+{
+        return (a % b);
+}
