966625a918b948d70fa91e678aeab2c2c64f4124
timmoh-king <timothykariuki43@gmail.com>
2023-07-13 14:00:01 +0300

    binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,4 +11,23 @@
 
 int binary_search(int *array, size_t size, int value)
 {
+	int mid;
+	size_t l = 0;
+	size_t r = size - 1;
+
+	if (array == NULL)
+		return (-1);
+
+	while (l <= r)
+	{
+		mid = ceil(l + (r - l) / 2);
+
+		if (array[mid] == value)
+			return (mid);
+		if (array[mid] <  value)
+			l = mid + 1;
+		else
+			r = mid - 1;
+	}
+	return (-1);
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,8 +2,7 @@
 #define ALGOS_HEADER
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
