6aa63ce9c80425bf99213f6f1deea357f57a2cd4
timmoh-king <timothykariuki43@gmail.com>
2023-07-13 12:26:58 +0300

    0-linear.c

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,14 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in an array of integers
+ * @array: pointer to the first element of the array to search
+ * @size: number of elements in array
+ * @value: the value to search for in the array
+ *
+ * Return: the first index where value is located
+*/
+
+int binary_search(int *array, size_t size, int value)
+{
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,5 +1,9 @@
-#ifndef "MAIN_H"
-#define "MAIN_H"
+#ifndef ALGOS_HEADER
+#define ALGOS_HEADER
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
